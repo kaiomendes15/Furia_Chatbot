c974e5517bac68bcdb62c29812d4d1bad1b5a0b6
Kaio <kakacnmendes@gmail.com>
2025-04-25 23:24:37 -0300

    main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import InputBox from './components/InputBox'
+
+import InputWithButton from './components/InputWithButton'
 import Container from './components/Container'
 import './App.css'
+import { useDarkMode } from './contexts/DarkModeContext'
+import Header from './components/Header'
 
 function App() {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  const theme = {
+    backgroundColor: darkMode ? "#1a1a1a" : "#f5f5f5", // Cinza escuro suave / Off-white
+    color: darkMode ? "#e0e0e0" : "#333333" // Cinza claro / Cinza escuro
+  };
+
   return (
-    <Container>
-      <InputBox />
+    <Container style={theme}>
+      <Header onChange={toggleDarkMode}/>
+      <InputWithButton/>
     </Container>
-  )
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { DarkModeProvider } from './contexts/DarkModeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App /> 
-    </Provider>
+    <DarkModeProvider>
+      <Provider>
+        <App /> 
+      </Provider>
+    </DarkModeProvider>
   </StrictMode>,
 )
